c4eeba01de586c7ac7fbb93b955d4bdd8550b94e
Alvin Garcia <agarcia1337@gmail.com>
2022-02-25 22:45:17 +0800

    Refactor airplane_seat

--- lib/seating_algorithm/airplane_seat.rb
+++ lib/seating_algorithm/airplane_seat.rb
@@ -26,21 +26,11 @@ module SeatingAlgorithm
     attr_accessor(*REQUIRED_FIELDS)
     attr_accessor :passenger, :name
 
-    def initialize args
-      REQUIRED_FIELDS.each do |key|
-        instance_variable_set("@#{key}", args.fetch(key))
-      end
-
-      raise(MaximumLimitReachedError, '`row` greater than 999 is not supported') if row > 999
-      raise(MaximumLimitReachedError, '`col` greater than 999 is not supported') if col > 999
-      raise(MaximumLimitReachedError, '`div` greater than 999 is not supported') if div > 999
-      raise(InvalidPosError, '`pos` only supports :window, :aisle, or :center') unless POSITION_WEIGHT.has_key?(pos)
+    def initialize ...
+      assign_fields(...)
+      validate_fields
 
       @name = [row, div, col, pos].join('-')
-
-    rescue KeyError => e
-      msg = e.message.sub('key not found: :', 'Must provide ')
-      raise MissingRequiredParameterError, msg
     end
 
     def seating_order
@@ -50,6 +40,24 @@ module SeatingAlgorithm
         weight += row * PRIORITY_WEIGHT[:row]
         weight += col * PRIORITY_WEIGHT[:col]
       end
+    end
+
+    private
+
+    def assign_fields args
+      REQUIRED_FIELDS.each do |key|
+        instance_variable_set("@#{key}", args.fetch(key))
+      end
+    rescue KeyError => e
+      msg = e.message.sub('key not found: :', 'Must provide ')
+      raise MissingRequiredParameterError, msg
+    end
+
+    def validate_fields
+      raise(MaximumLimitReachedError, '`row` greater than 999 is not supported') if row > 999
+      raise(MaximumLimitReachedError, '`col` greater than 999 is not supported') if col > 999
+      raise(MaximumLimitReachedError, '`div` greater than 999 is not supported') if div > 999
+      raise(InvalidPosError, '`pos` only supports :window, :aisle, or :center') unless POSITION_WEIGHT.has_key?(pos)
     end
 
   end
